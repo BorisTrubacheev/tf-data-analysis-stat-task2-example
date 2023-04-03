e0faf29f476c6047298acf8617ced65468da6d57
BorisTrubacheev <96075379+BorisTrubacheev@users.noreply.github.com>
2023-04-03 14:37:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,5 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return -2/(17**2)*(expon.ppf(alpha / 2) + x.mean() + 0.5), \
-           -2/(17**2)*(expon.ppf(1 - alpha / 2) + x.mean() + 0.5)
+    return 2/(17**2)*(scipy.stats.expon.ppf(alpha / 2) + x.mean() - 0.5), \
+           2/(17**2)*(scipy.stats.expon.ppf(1 - alpha / 2) + x.mean() - 0.5)
